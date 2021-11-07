011ad2c5488b044ed16cd7a6a136e08f1bb4f3c8
burak-altin <burak.altin@steadfasttech.com.au>
2021-11-07 14:26:33 +1100

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+function saturdayFun(event="roller-skate") {
+    return `This Saturday, I want to ${event}!`;
+}
+
+function mondayWork(event="go to the office"){
+    return `This Monday, I will ${event}.`;
+}
+
+function wrapAdjective(value1="*"){
+    return function(value2="special"){
+        return `You are ${value1}${value2}${value1}!`;
+
+    }
+}
+
+let Calculator = { 
+    add : function() { return 1+3; } , 
+    subtract : function() { return 1-3; } ,
+    multiply : function() { return 1*3; } ,
+    divide : function() { return 10/5; }
+};
+
+function actionApplyer(number, arr) {
+    if (arr.length === 0){
+        return number;
+    }
+    return arr[2](arr[1](arr[0](number)));
+
+};
